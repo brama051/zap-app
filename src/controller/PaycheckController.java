cf1056ed28235de3c47ee597080ad5a31b44ecae
Edi Ibriks <edi_137@msn.com>
2017-01-21 22:58:32 +0100

    gotov paycheckController

--- src/controller/PaycheckController.java
+++ src/controller/PaycheckController.java
@@ -1,6 +1,12 @@
 package controller;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
 
 import model.Paycheck;
 
@@ -13,18 +19,70 @@ public class PaycheckController {
 		this.paycheckList = new ArrayList<Paycheck>();
 	}
 	
-	public void createPaycheck(Paycheck placa){}
+	public int lastIndex(){
+		return this.paycheckList.size();
+	}
 	
-	public void readPaycheck(int id){}
+	public void createPaycheck(Paycheck paycheck){
+		this.paycheckList.add(paycheck);
+	}
 	
-	public void updatePaycheck(Paycheck paycheck){}
+	public Paycheck readPaycheck(int id){
+		for (Paycheck object: this.paycheckList)
+		    if (object.getId() == id) return object;
+		return null;
+	}
 	
-	public void deletePaycheck(int id){}
+	public void updatePaycheck(Paycheck paycheck){
+		int i = 0;
+		for (Paycheck object: this.paycheckList){
+		    if (object.getId() == paycheck.getId()){
+		    	this.paycheckList.get(i).setFirstName(paycheck.getFirstName());
+		    	this.paycheckList.get(i).setLastName(paycheck.getLastName());
+		    	this.paycheckList.get(i).setMonth(paycheck.getMonth());
+		    	this.paycheckList.get(i).setPricePerHour(paycheck.getPricePerHour());
+		    	this.paycheckList.get(i).setTotalHours(paycheck.getTotalHours());
+		    }
+		    i++;
+		}
+	}
 	
-	public void load(){}
+	public void deletePaycheck(int id){
+		for (Paycheck object: this.paycheckList)
+		    if (object.getId() == id) this.paycheckList.remove(object);
+	}
 	
-	public void save(){}
+	public void load() throws IOException{
+		
+		CSVReader reader = new CSVReader(new FileReader(this.fileName));
+	    String[] nextLine = new String[7];
+	    while ((nextLine = reader.readNext()) != null) {
+	        Paycheck paycheck = new Paycheck(Integer.parseInt(nextLine[0]), nextLine[1], nextLine[2], 
+	        		Integer.parseInt(nextLine[3]), Float.parseFloat(nextLine[4]), Float.parseFloat(nextLine[5]));
+	        paycheckList.add(paycheck);
+	    }
+	}
 	
+	public void save() throws IOException{
+		CSVWriter writer = new CSVWriter(new FileWriter(this.fileName));
+		//List<String> lines = new ArrayList<String>();
+		for (Paycheck object: this.paycheckList){
+			String[] entries = new String[7];
+			entries[0] = Integer.toString(object.getId());
+			entries[1] = object.getFirstName();
+			entries[2] = object.getLastName();
+			entries[3] = Integer.toString(object.getMonth());
+			entries[4] = Float.toString(object.getPricePerHour());
+			entries[5] = Float.toString(object.getTotalHours());
+			entries[6] = Float.toString(object.getTotal());
+			writer.writeNext(entries);	
+		}
+		writer.close();
+	}
 	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello novi dokument");
+	}
 	
 }
--- src/model/Paycheck.java
+++ src/model/Paycheck.java
@@ -16,7 +16,7 @@ public class Paycheck {
 	private float pricePerHour;
 	/*total */
 	
-	Paycheck(){
+	public Paycheck(){
 		this.id = -1;
 		this.firstName = "";
 		this.lastName = "";
@@ -25,7 +25,7 @@ public class Paycheck {
 		this.pricePerHour = 0;
 	}
 	
-	Paycheck(int id, String firstName, String lastName, int month, float totalHours, float pricePerHour){
+	public Paycheck(int id, String firstName, String lastName, int month, float totalHours, float pricePerHour){
 		this.id = id;
 		this.firstName = firstName;
 		this.lastName = lastName;
@@ -34,8 +34,56 @@ public class Paycheck {
 		this.pricePerHour = pricePerHour;
 	}
 	
-	float getTotal(){
+	public float getTotal(){
 		return this.pricePerHour * this.totalHours;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getMonth() {
+		return month;
+	}
+
+	public void setMonth(int month) {
+		this.month = month;
+	}
+
+	public float getTotalHours() {
+		return totalHours;
+	}
+
+	public void setTotalHours(float totalHours) {
+		this.totalHours = totalHours;
+	}
+
+	public float getPricePerHour() {
+		return pricePerHour;
+	}
+
+	public void setPricePerHour(float pricePerHour) {
+		this.pricePerHour = pricePerHour;
 	}
 	
 }
