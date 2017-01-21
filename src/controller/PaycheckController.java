374e472d759cc6765bc146b0579d0b4644799582
Vedran Abramovic <vedran.abramovic@gmail.com>
2017-01-21 20:27:00 +0100

    Contructors added

--- src/controller/PaycheckController.java
+++ src/controller/PaycheckController.java
@@ -5,11 +5,12 @@ import java.util.ArrayList;
 import model.Paycheck;
 
 public class PaycheckController {
-	public ArrayList<Paycheck> zapList;
+	public ArrayList<Paycheck> paycheckList;
 	private String fileName;
 	
 	PaycheckController(String fileName){
 		this.fileName = fileName;
+		this.paycheckList = new ArrayList<Paycheck>();
 	}
 	
 	public void createPaycheck(Paycheck placa){}
--- src/model/Paycheck.java
+++ src/model/Paycheck.java
@@ -12,5 +12,30 @@ public class Paycheck {
 	private String firstName;
 	private String lastName;
 	private int month;
+	private float totalHours;
+	private float pricePerHour;
+	/*total */
+	
+	Paycheck(){
+		this.id = -1;
+		this.firstName = "";
+		this.lastName = "";
+		this.month = 0;
+		this.totalHours = 0;
+		this.pricePerHour = 0;
+	}
+	
+	Paycheck(int id, String firstName, String lastName, int month, float totalHours, float pricePerHour){
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.month = month;
+		this.totalHours = totalHours;
+		this.pricePerHour = pricePerHour;
+	}
+	
+	float getTotal(){
+		return this.pricePerHour * this.totalHours;
+	}
 	
 }
