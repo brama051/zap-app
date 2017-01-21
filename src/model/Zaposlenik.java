1d6c57802c1af446ed48e41d7e51a1cf5dd72f88
Vedran Abramovic <vedran.abramovic@gmail.com>
2017-01-21 17:24:11 +0100

    Inital commit

---
+++ src/model/Zaposlenik.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package model;
+
+/**
+ * @author brama
+ *
+ */
+public class Zaposlenik {
+	private int id;
+	private String firstName;
+	private String lastName;
+	private int month;
+	
+}
